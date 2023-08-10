dcadd93ac9be74677a08281ed62c673a17ccdbce
Dobbiejr <dobbinsjrkachitsa@gmail.com>
2023-08-10 13:19:10 +0200

    update

--- resources/views/livewire/layout/aside/left-nav-livewire.blade.php
+++ resources/views/livewire/layout/aside/left-nav-livewire.blade.php
@@ -13,7 +13,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img @if( Auth::user()->profile_picture == "") src="{{ asset('assets/uploads/' . Auth::user()->profile_picture) }}"  @else src="{{ asset('face-0.jpg') }} @endif
+                    <img @if( Auth::user()->profile_picture == "") src="{{ asset('assets/uploads/' . Auth::user()->profile_picture) }}"  @else src="{{ asset('face-0.jpg') }}" @endif
                         class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
