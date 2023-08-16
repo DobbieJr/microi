cfa462944a61b19771659ef7fcc3f6762ef7ee55
Dobbiejr <dobbinsjrkachitsa@gmail.com>
2023-08-16 16:54:23 +0200

    update

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LandingController extends Controller
 {
     //
-public function index(){
-return view('landing');
-}
+    public function index()
+    {
+        $u =  User::where('role','admin')->get();
+        dd($us);
+        return view('landing');
+    }
 }
